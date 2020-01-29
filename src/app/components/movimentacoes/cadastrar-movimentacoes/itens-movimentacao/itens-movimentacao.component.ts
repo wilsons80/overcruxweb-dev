6c06162d404fbd2a3684ae785f2d59fb80590ff9
Josue Rodrigues <josuerodrigues@MacBook.local>
2020-01-28 23:27:42 -0300

    WIP - Itens Movimentacao

--- src/app/components/movimentacoes/cadastrar-movimentacoes/itens-movimentacao/itens-movimentacao.component.ts
+++ src/app/components/movimentacoes/cadastrar-movimentacoes/itens-movimentacao/itens-movimentacao.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItensMovimentacaoComponent implements OnInit {
 
+
+  materiais = [{nome: "Material 1"}, {nome: "Material 2"}];
+  categorias = [{nome: "Categoria 1"}, {nome: "Categoria 2"}];
+  pedidosMaterial = [{nome: "Pedido 1"}, {nome: "Pedido 2"}];
+
+  valorUnitario:number;
+  valorTotal:number;
+  quantidade:number;
+
   constructor() { }
 
   ngOnInit() {
