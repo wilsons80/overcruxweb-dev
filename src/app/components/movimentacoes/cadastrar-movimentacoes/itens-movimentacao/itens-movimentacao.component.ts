ab80afac87075db44c072d87fef33e99569c2435
josue.rodrigues <josue.rodrigues@mpdft.mp.br>
2020-01-30 14:45:52 -0300

    Fazendo o itens como lista

--- src/app/components/movimentacoes/cadastrar-movimentacoes/itens-movimentacao/itens-movimentacao.component.ts
+++ src/app/components/movimentacoes/cadastrar-movimentacoes/itens-movimentacao/itens-movimentacao.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { Acesso } from 'src/app/core/acesso';
+import { ToastService } from 'src/app/services/toast/toast.service';
+import { ItemMovimentacao } from 'src/app/core/item-movimentacao';
 
 @Component({
   selector: 'itens-movimentacao',
@@ -7,18 +11,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItensMovimentacaoComponent implements OnInit {
 
+  @Input() listaItemMovimentacao: ItemMovimentacao[] = [];
 
-  materiais = [{nome: "Material 1"}, {nome: "Material 2"}];
-  categorias = [{nome: "Categoria 1"}, {nome: "Categoria 2"}];
-  pedidosMaterial = [{nome: "Pedido 1"}, {nome: "Pedido 2"}];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  valorUnitario:number;
-  valorTotal:number;
-  quantidade:number;
+  mostrarTabela = true;
+  msg: string;
 
-  constructor() { }
+  materiais = [{ nome: "Material 1" }, { nome: "Material 2" }];
+  categorias = [{ nome: "Categoria 1" }, { nome: "Categoria 2" }];
+  pedidosMaterial = [{ nome: "Pedido 1" }, { nome: "Pedido 2" }];
+
+  
+  displayedColumns: string[] = ['categoria','material', 'qtdMaterial', 'valorUnitarioItem', 'valorTotalItem'];
+  dataSource: MatTableDataSource<ItemMovimentacao> = new MatTableDataSource();
+
+ 
+
+  itemMovimentacao: ItemMovimentacao;
+
+  perfilAcesso: Acesso;
+
+  openFormCadastro = false;
+  isAtualizar = false;
+
+  valorUnitario: number = 0;
+  valorTotal: number = 0;
+  quantidade: number = 0;
+
+  constructor(
+  ) { }
 
   ngOnInit() {
+
+    this.inicializaLista();
   }
+
+  initObjeto() {}
+
+  limpar() { }
+
+  adicionar() {}
+
+  getObjetosCompletosParaLista(itemMovimentacao: ItemMovimentacao) {}
+
+  atualizar() { }
+
+
+  inicializaLista(){
+    const item = new ItemMovimentacao();
+    item.categoria = "Categoria 1";
+    item.material = "Material 1"
+    item.qtdMaterial = 3;
+    item.valorUnitarioItem = 5;
+    item.valorTotalItem = 15;
+
+    const item1 = new ItemMovimentacao();
+    item1.categoria = "Categoria 2";
+    item1.material = "Material 2"
+    item1.qtdMaterial = 10;
+    item1.valorUnitarioItem = 5;
+    item1.valorTotalItem = 50;
+
+    this.listaItemMovimentacao.push(item);
+    this.listaItemMovimentacao.push(item1);
+
+    this.dataSource.data = this.listaItemMovimentacao;
+  }
+
+  carregarLista(){}
+
 
 }
---
+++ src/app/core/item-movimentacao.ts
@@ -0,0 +1,11 @@
+export class ItemMovimentacao{
+    id:any     
+    movimentacao:any         
+    material:any          
+    categoria:any         
+    unidade:any           
+    pedidoMaterial:any       
+    qtdMaterial:any          
+    valorUnitarioItem :any   
+    valorTotalItem:any       
+}
