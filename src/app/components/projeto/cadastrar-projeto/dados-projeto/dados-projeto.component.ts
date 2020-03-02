4d4217e1502eac95353c4ee2eeb517b05f3c9417
Wilson Souza <wilsons80@gmail.com>
2020-03-02 10:54:20 -0300

    Inclusao do campo programa na tela de projetos

--- src/app/components/projeto/cadastrar-projeto/dados-projeto/dados-projeto.component.ts
+++ src/app/components/projeto/cadastrar-projeto/dados-projeto/dados-projeto.component.ts
@@ -4,6 +4,8 @@ import { Projeto } from 'src/app/core/projeto';
 import { FuncionarioService } from 'src/app/services/funcionario/funcionario.service';
 import { Funcionario } from 'src/app/core/funcionario';
 import * as _ from 'lodash';
+import { Programa } from 'src/app/core/programa';
+import { ProgramaService } from 'src/app/services/programa/programa.service';
 
 @Component({
   selector: 'dados-projeto',
@@ -16,13 +18,27 @@ export class DadosProjetoComponent implements OnInit {
   @Input() projeto: Projeto;
 
   funcionarios: Funcionario[];
+  programas: Programa[];
 
-  constructor(private funcionarioService: FuncionarioService) { }
+  constructor(private funcionarioService: FuncionarioService,
+              private programaService: ProgramaService) {
+  }
 
   ngOnInit() {
     this.funcionarioService.getAll().subscribe((funcionarios: Funcionario[]) => {
       this.funcionarios = funcionarios;
     });
+
+    this.programaService.getAllProgramasIntituicaoLogada().subscribe((programas: Programa[]) => {
+      this.programas = programas;
+    });
+
+  }
+
+  carregarDadosPrograma() {
+    if (this.projeto.programa.id) {
+      this.projeto.programa = _.cloneDeep(_.find(this.programas,  (f: Programa) => f.id === this.projeto.programa.id));
+    }
   }
 
 }
