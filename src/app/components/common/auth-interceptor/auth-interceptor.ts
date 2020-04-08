01cbb7b47885a5d6cab80aa04231e81676d8a8e7
Wilson Carvalho Souza <wilson.carvalho@mpdft.mp.br>
2020-04-08 15:41:59 -0300

    Ajuste ao passar o IDSESSAOUSUARIO nas requisições

--- src/app/components/common/auth-interceptor/auth-interceptor.ts
+++ src/app/components/common/auth-interceptor/auth-interceptor.ts
@@ -8,11 +8,13 @@ import { Observable } from 'rxjs';
 export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
-
+    const token            = localStorage.getItem('token');
+    const idsessionusuario = localStorage.getItem('IDSESSIONUSUARIO');
+  
     if (token) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', 'Bearer '.concat(token))
+                            .set('IDSESSIONUSUARIO', idsessionusuario)
       });
 
       return next.handle(cloned);
