a8c8ea49d9dfb4262e3cb5060800455101b40949
Wilson Souza <wilsons80@gmail.com>
2021-09-29 14:35:34 -0300

    Testando proxy na AWS

--- proxy.config.js
+++ proxy.config.js
@@ -1,4 +1,5 @@
-const targetDomain = process.env.NODE_ENV? `http://www.api-dev.s3manager.com.br` : 'http://localhost:8080';
+//const targetDomain = process.env.NODE_ENV? `http://www.api-dev.s3manager.com.br` : 'http://localhost:8080';
+const targetDomain = process.env.NODE_ENV? `http://54.160.45.246` : 'http://localhost:8080';
 console.log(`Usando o endereço "${targetDomain}" para o proxy...\n`);
 
 
